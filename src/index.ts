b711e031c7ace55767845db56372251be86c7aff
yuan <1194423126@qq.com>
2025-05-29 00:38:00 +0800

    测试邮件发送功能

--- src/index.ts
+++ src/index.ts
@@ -9,31 +9,31 @@ type Bindings = {
 
 const app = new Hono<{ Bindings: Bindings }>()
 
-app.get('/', (c) => {
+app.get('/', async (c) => {
   const transporter = nodemailer.createTransport({
-      host: "smtp.qq.com",
-      port: 465,
-      auth: {
-        user: c.env.EMAIL_ROBOT,
-        pass: c.env.EMAIL_AUTH_CODE
-      }
+    host: "smtp.qq.com",
+    port: 465,
+    auth: {
+      user: c.env.EMAIL_ROBOT,
+      pass: c.env.EMAIL_AUTH_CODE
+    }
+  });
+  console.log("邮箱配置成功");
+  try {
+    const info = await transporter.sendMail({
+      from: "robot<" + c.env.EMAIL_ROBOT + ">",
+      to: "770720805@qq.com, 1194423126@qq.com",
+      subject: "Hello",
+      text: "Hello world?",
+      html: "<b>Hello world?</b>",
     });
-    (async () => {
-      try {
-        const info = await transporter.sendMail({
-          from: "robot<" + c.env.EMAIL_ROBOT + ">",
-          to: "770720805@qq.com, 1194423126@qq.com",
-          subject: "Hello",
-          text: "Hello world?",
-          html: "<b>Hello world?</b>",
-        });
-        console.log("Message sent: %s", info.messageId);
-        console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
-      } catch (err) {
-        console.error("Error while sending mail", err);
-      }
-    })();
-  return c.text('Hello Hono!')
+    console.log("Message sent: %s", info.messageId);
+    console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+    return c.text('Hello Hono!ok')
+  } catch (err) {
+    console.error("Error while sending mail", err);
+    return c.text('Hello Hono!err')
+  }
 })
 
 interface EmailEnv extends Env {
@@ -46,7 +46,7 @@ export default {
     return app.fetch(request, env, ctx)
   },
 
-  async scheduled(env:  EmailEnv) {
+  async scheduled(env: EmailEnv) {
     const transporter = nodemailer.createTransport({
       host: "smtp.qq.com",
       port: 465,
