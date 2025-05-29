4e48addc79aa7ea70b30199cdc040dc8eccfc503
yuan <1194423126@qq.com>
2025-05-29 08:02:32 +0800

    测试邮件发送功能

--- src/index.ts
+++ src/index.ts
@@ -1,39 +1,10 @@
 import { Env, ExecutionContext, Hono } from 'hono'
 import nodemailer from 'nodemailer'
 
-
-type Bindings = {
-  EMAIL_ROBOT: string
-  EMAIL_AUTH_CODE: string
-}
-
-const app = new Hono<{ Bindings: Bindings }>()
+const app = new Hono()
 
 app.get('/', async (c) => {
-  const transporter = nodemailer.createTransport({
-    host: "smtp.qq.com",
-    port: 465,
-    auth: {
-      user: c.env.EMAIL_ROBOT,
-      pass: c.env.EMAIL_AUTH_CODE
-    }
-  });
-  console.log("邮箱配置成功");
-  try {
-    const info = await transporter.sendMail({
-      from: "robot<" + c.env.EMAIL_ROBOT + ">",
-      to: "770720805@qq.com, 1194423126@qq.com",
-      subject: "Hello",
-      text: "Hello world?",
-      html: "<b>Hello world?</b>",
-    });
-    console.log("Message sent: %s", info.messageId);
-    console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
-    return c.text('Hello Hono!ok')
-  } catch (err) {
-    console.error("Error while sending mail", err);
-    return c.text('Hello Hono!err')
-  }
+      return c.text('Hello Hono!')
 })
 
 interface EmailEnv extends Env {
@@ -48,27 +19,26 @@ export default {
 
   async scheduled(env: EmailEnv) {
     const transporter = nodemailer.createTransport({
-      host: "smtp.qq.com",
-      port: 465,
-      auth: {
-        user: env.EMAIL_ROBOT,
-        pass: env.EMAIL_AUTH_CODE
-      }
+    host: "smtp.qq.com",
+    port: 465,
+    auth: {
+      user: env.EMAIL_ROBOT,
+      pass: env.EMAIL_AUTH_CODE
+    }
+  });
+  console.log("邮箱配置成功");
+  try {
+    const info = await transporter.sendMail({
+      from: "robot<" + env.EMAIL_ROBOT + ">",
+      to: "770720805@qq.com, 1194423126@qq.com",
+      subject: "Hello",
+      text: "Hello world?",
+      html: "<b>Hello world?</b>",
     });
-    (async () => {
-      try {
-        const info = await transporter.sendMail({
-          from: "robot<" + env.EMAIL_ROBOT + ">",
-          to: "770720805@qq.com, 1194423126@qq.com",
-          subject: "Hello",
-          text: "Hello world?",
-          html: "<b>Hello world?</b>",
-        });
-        console.log("Message sent: %s", info.messageId);
-        console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
-      } catch (err) {
-        console.error("Error while sending mail", err);
-      }
-    })();
+    console.log("Message sent: %s", info.messageId);
+    console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+  } catch (err) {
+    console.error("Error while sending mail", err);
+  }
   },
 }
