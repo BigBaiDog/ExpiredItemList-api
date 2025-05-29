faa026346b56e829e1584af5fab21570863d0f1d
yuan <1194423126@qq.com>
2025-05-29 21:42:15 +0800

    测试邮件发送功能

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,11 @@
 import { ExecutionContext, Hono } from 'hono'
 import nodemailer from 'nodemailer'
+import { ScheduledController } from '@cloudflare/workers-types'
 
 const app = new Hono()
 
 app.get('/', async (c) => {
-      return c.text('Hello Hono!')
+  return c.text('Hello Hono!')
 })
 
 interface Env {
@@ -17,30 +18,30 @@ export default {
     return app.fetch(request, env, ctx)
   },
 
-  async scheduled(env: Env) {
+  async scheduled(controller: ScheduledController, env: Env, ctx: ExecutionContext,) {
     const transporter = nodemailer.createTransport({
-    host: "smtp.qq.com",
-    port: 465,
-    auth: {
-      user: env.EMAIL_ROBOT,
-      pass: env.EMAIL_AUTH_CODE
-    }
-  });
-  console.log(env.EMAIL_ROBOT)
-  console.log(env.EMAIL_AUTH_CODE)
-  console.log("邮箱配置成功");
-  try {
-    const info = await transporter.sendMail({
-      from: "robot<" + env.EMAIL_ROBOT + ">",
-      to: "770720805@qq.com, 1194423126@qq.com",
-      subject: "Hello",
-      text: "Hello world?text",
-      html: "<b>Hello world?html</b>",
+      host: "smtp.qq.com",
+      port: 465,
+      auth: {
+        user: env.EMAIL_ROBOT,
+        pass: env.EMAIL_AUTH_CODE
+      }
     });
-    console.log("Message sent: %s", info.messageId);
-    console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
-  } catch (err) {
-    console.error("Error while sending mail", err);
-  }
+    console.log(env.EMAIL_ROBOT)
+    console.log(env.EMAIL_AUTH_CODE)
+    console.log("邮箱配置成功");
+    try {
+      const info = await transporter.sendMail({
+        from: "robot<" + env.EMAIL_ROBOT + ">",
+        to: "770720805@qq.com, 1194423126@qq.com",
+        subject: "Hello",
+        text: "Hello world?text",
+        html: "<b>Hello world?html</b>",
+      });
+      console.log("Message sent: %s", info.messageId);
+      console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+    } catch (err) {
+      console.error("Error while sending mail", err);
+    }
   },
 }
