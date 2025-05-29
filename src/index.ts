2fb8b52fc10a54ee09bde8bf49d92d355d2e94b7
yuan <1194423126@qq.com>
2025-05-29 21:13:54 +0800

    测试邮件发送功能

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { Env, ExecutionContext, Hono } from 'hono'
+import { ExecutionContext, Hono } from 'hono'
 import nodemailer from 'nodemailer'
 
 const app = new Hono()
@@ -7,7 +7,7 @@ app.get('/', async (c) => {
       return c.text('Hello Hono!')
 })
 
-interface EmailEnv extends Env {
+interface Env {
   EMAIL_ROBOT: string;
   EMAIL_AUTH_CODE: string;
 }
@@ -17,7 +17,7 @@ export default {
     return app.fetch(request, env, ctx)
   },
 
-  async scheduled(env: EmailEnv) {
+  async scheduled(env: Env) {
     const transporter = nodemailer.createTransport({
     host: "smtp.qq.com",
     port: 465,
@@ -32,8 +32,8 @@ export default {
       from: "robot<" + env.EMAIL_ROBOT + ">",
       to: "770720805@qq.com, 1194423126@qq.com",
       subject: "Hello",
-      text: "Hello world?",
-      html: "<b>Hello world?</b>",
+      text: "Hello world?text",
+      html: "<b>Hello world?html</b>",
     });
     console.log("Message sent: %s", info.messageId);
     console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
