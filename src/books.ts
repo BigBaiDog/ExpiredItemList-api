8883f644a3dca1735c6a60560077b4af7637fe6d
BigBaiDog <1194423126@qq.com>
2025-06-02 20:17:35 +0800

    连接数据库

--- src/books.ts
+++ src/books.ts
@@ -1,10 +1,38 @@
 // books.ts
-import { Hono } from 'hono'
+import { Hono } from "hono"
+import { env } from "hono/adapter"
+import { drizzle } from "drizzle-orm/mysql2"
+import { createConnection } from "mysql2"
+import { pendingNotification } from "./schema"
+
+interface HYPERDRIVE {
+  host: string;
+  user: string;
+  password: string;
+  database: string;
+  port: number;
+}
 
 const app = new Hono()
 
-app.get('/', (c) => c.json('list books'))
-app.post('/', (c) => c.json('create a book', 201))
-app.get('/:id', (c) => c.json(`get ${c.req.param('id')}`))
+app.get("/", async (c) => {
+  const hyperdriveEnv = env<{ HYPERDRIVE: HYPERDRIVE }>(c).HYPERDRIVE
+  console.log(hyperdriveEnv)
+  const connection = createConnection({
+                                        host: hyperdriveEnv.host,
+                                        user: hyperdriveEnv.user,
+                                        password: hyperdriveEnv.password,
+                                        database: hyperdriveEnv.database,
+                                        port: hyperdriveEnv.port,
+                                        disableEval: true
+                                      })
+  const db = drizzle(connection)
+  console.log(db)
+  const aa: any[] = await db.select().from(pendingNotification)
+
+  return c.json(aa)
+})
+app.post("/", (c) => c.json("create a book", 201))
+app.get("/:id", (c) => c.json(`get ${c.req.param("id")}`))
 
 export default app
--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,13 @@
-import { Hono } from 'hono'
-import nodemailer from 'nodemailer'
-import { ScheduledController } from '@cloudflare/workers-types'
-import authors from './authors'
-import books from './books'
+import { Hono } from "hono"
+import nodemailer from "nodemailer"
+import { ScheduledController } from "@cloudflare/workers-types"
+import authors from "./authors"
+import books from "./books"
 
 const app = new Hono()
 
-app.route('/authors', authors)
-app.route('/books', books)
+app.route("/authors", authors)
+app.route("/books", books)
 
 interface Env {
   EMAIL_ROBOT: string;
@@ -18,30 +18,30 @@ export default {
   fetch: app.fetch,
   scheduled: async (controller: ScheduledController, env: Env) => {
     const transporter = nodemailer.createTransport({
-      host: "smtp.qq.com",
-      port: 465,
-      auth: {
-        user: env.EMAIL_ROBOT,
-        pass: env.EMAIL_AUTH_CODE
-      }
-    });
+                                                     host: "smtp.qq.com",
+                                                     port: 465,
+                                                     auth: {
+                                                       user: env.EMAIL_ROBOT,
+                                                       pass: env.EMAIL_AUTH_CODE
+                                                     }
+                                                   })
     console.log(env.EMAIL_ROBOT)
     console.log(env.EMAIL_AUTH_CODE)
-    console.log("邮箱配置成功");
+    console.log("邮箱配置成功")
     try {
       const info = await transporter.sendMail({
-        from: "robot<" + env.EMAIL_ROBOT + ">",
-        to: "770720805@qq.com, 1194423126@qq.com",
-        subject: "Hello",
-        text: "Hello world?text",
-        html: "<b>Hello world?html</b>",
-      });
-      console.log("Message sent: %s", info.messageId);
-      console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+                                                from: "robot<" + env.EMAIL_ROBOT + ">",
+                                                to: "770720805@qq.com, 1194423126@qq.com",
+                                                subject: "Hello",
+                                                text: "Hello world?text",
+                                                html: "<b>Hello world?html</b>"
+                                              })
+      console.log("Message sent: %s", info.messageId)
+      console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info))
     } catch (err) {
-      console.error("Error while sending mail", err);
+      console.error("Error while sending mail", err)
     } finally {
-      transporter.removeAllListeners(); // 显式关闭 
+      transporter.removeAllListeners() // 显式关闭
     }
-  },
+  }
 }
