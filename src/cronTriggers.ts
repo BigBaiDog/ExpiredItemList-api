9af9c662cba9482ed553945bf8f831bf5cb32aa2
BigBaiDog <1194423126@qq.com>
2025-06-02 16:42:28 +0800

    分离定时器文件

---
+++ src/cronTriggers.ts
@@ -0,0 +1,35 @@
+import { ScheduledController } from '@cloudflare/workers-types'
+import nodemailer from "nodemailer";
+
+interface Env {
+    EMAIL_ROBOT: string;
+    EMAIL_AUTH_CODE: string;
+}
+export async function sendMailMessage(env: Env){
+        const transporter = nodemailer.createTransport({
+            host: "smtp.qq.com",
+            port: 465,
+            auth: {
+                user: env.EMAIL_ROBOT,
+                pass: env.EMAIL_AUTH_CODE
+            }
+        });
+        console.log(env.EMAIL_ROBOT)
+        console.log(env.EMAIL_AUTH_CODE)
+        console.log("邮箱配置成功");
+        try {
+            const info = await transporter.sendMail({
+                from: "robot<" + env.EMAIL_ROBOT + ">",
+                to: "1194423126@qq.com",
+                subject: "Hello",
+                text: "Hello world?text",
+                html: "<b>Hello world?html</b>",
+            });
+            console.log("Message sent: %s", info.messageId);
+            console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+        } catch (err) {
+            console.error("Error while sending mail", err);
+        } finally {
+            transporter.removeAllListeners(); // 显式关闭
+        }
+    }
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,12 @@
-import { Hono } from 'hono'
+import {Env, Hono} from 'hono'
 import nodemailer from 'nodemailer'
-import { ScheduledController } from '@cloudflare/workers-types'
+// import { ScheduledController } from '@cloudflare/workers-types'
 import authors from './authors'
 import books from './books'
+import {sendMailMessage} from './cronTriggers'
 
 const app = new Hono()
-
+// const send = new sendMailMessage()
 app.route('/authors', authors)
 app.route('/books', books)
 
@@ -16,32 +17,7 @@ interface Env {
 
 export default {
   fetch: app.fetch,
-  scheduled: async (controller: ScheduledController, env: Env) => {
-    const transporter = nodemailer.createTransport({
-      host: "smtp.qq.com",
-      port: 465,
-      auth: {
-        user: env.EMAIL_ROBOT,
-        pass: env.EMAIL_AUTH_CODE
-      }
-    });
-    console.log(env.EMAIL_ROBOT)
-    console.log(env.EMAIL_AUTH_CODE)
-    console.log("邮箱配置成功");
-    try {
-      const info = await transporter.sendMail({
-        from: "robot<" + env.EMAIL_ROBOT + ">",
-        to: "770720805@qq.com, 1194423126@qq.com",
-        subject: "Hello",
-        text: "Hello world?text",
-        html: "<b>Hello world?html</b>",
-      });
-      console.log("Message sent: %s", info.messageId);
-      console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
-    } catch (err) {
-      console.error("Error while sending mail", err);
-    } finally {
-      transporter.removeAllListeners(); // 显式关闭 
-    }
-  },
+  scheduled: (event: any, env: Env) => {
+    sendMailMessage(env)
+  }
 }
